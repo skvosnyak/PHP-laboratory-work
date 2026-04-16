a7349c8a8cfdabf9caab8cb059adfc1b62e0a2ea
skvosnyak <zardim2004@gmail.com>
2026-04-16 10:54:23 +0300

    Lab1 (hello world page)
    # Conflicts:
    #	.gitignore

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<?php
+date_default_timezone_set('Europe/Moscow');
+$title = "Задание: Hello, World!";
+$currentTime = date("d.m.Y H:i:s"); 
+$helloMessage = "Hello, World!";
+
+$hour = date("H");
+if ($hour < 12) {
+    $greeting = "Доброе утро";
+} elseif ($hour < 18) {
+    $greeting = "Добрый день";
+} else {
+    $greeting = "Добрый вечер";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="/styles/welcome.css">
+</head>
+<body>
+    <div class="header">
+        <img class="logo" src="/src/polytech_logo_main_RGB.png">
+        <div class="title"><?= $title ?></div>
+        <div style="width: 100px;"></div>
+    </div>
+
+    <div class="main">
+        <div class="content">
+            <div class="greeting"><?=$greeting?>!</div>
+            <div class="hello"><?=$helloMessage?></div>
+            <div class="time">Серверное время: <?= $currentTime ?></div>
+        </div>
+    </div>
+
+    <div class="footer">
+      Задание для самостоятельной работы
+    </div>
+</body>
+</html>
